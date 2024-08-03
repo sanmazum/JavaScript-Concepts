1c53c59f42a9ea2060efeb66650f997a45c3da5f
Sandeepan Mazumdar <155097600+sanmazum@users.noreply.github.com>
2024-08-03 22:28:21 +0000

    Operations on conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,6 +28,36 @@ console.log(booleanIsLoggedIn);    // prints 'true'
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+console.log(stringNumber); // prints -> 33
+console.log(typeof stringNumber)  // prints -> string
 
+// ********************** Operations ********************************** 
+
+let value = 3 
+let negValue = -value
+console.log(negValue) // prints -> -3
+
+let str1 = "Hello"
+let str2 = " sandeepan"
+
+let str3 = str1 + str2
+
+console.log(str3)  // Prints -> Hello sandeepan
+
+
+// But Problem occurs here ->
+
+console.log("1" + 2);  // Prints -> 12 😒
+console.log("1" + 2 + 2); // Prints -> 122 🤔 (string taken and the numbers weren't added, just got attached one by one. strange behaviour)
+console.log( 1+ 2 + "1"); // Prints -> 32 🤯 ( first added and then string got attached)  -> All these are governed by ECMAScript guidelines.
+
+console.log( +true) // prints -> 1
+
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2+2 
+
+let gameCounter = 100
+gameCounter++  
+console.log(gameCounter); // Prints -> 101 
