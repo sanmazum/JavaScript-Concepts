77b2a74026c926ca9ec2e261b4c61db40e3d13eb
Sandeepan Mazumdar <155097600+sanmazum@users.noreply.github.com>
2024-08-03 21:45:46 +0000

    Datatypes Discussion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use Strict" // treat all JS code as newer version
+
+/* alert(3+3) - we are using node js and not browser.
+ In Browser you will get a alert message with 6. But doesn't happen in Node 
+ */
+
+let name = "Sandeepan"
+let age = 26
+let isLoggedIn = false
+
+//Primitive DataTypes :- 
+//number => 2 to power 53
+//Bigint
+//String => ""
+//boolean => true / false
+//null => standalone value, it's a representation of empty value.
+//undefined => when value is not assigned, but variable is declared
+//symbol=> helps in uniqueness of components 
+
+
+//Objects
+
+console.log(typeof "")
+console.log(typeof null);  // prints 'Object' - people call it a flaw
+console.log(typeof undefined);  // prints undefined
